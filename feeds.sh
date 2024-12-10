9aae176cb11426b78ecd0fb688a44e05269eaa3c
AllsVoid <58662058+AllsVoid@users.noreply.github.com>
2024-12-10 17:49:42 +0800

    Update feeds.sh

--- feeds.sh
+++ feeds.sh
@@ -5,4 +5,4 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
+# git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
